9d1fa1196e78a794d8df266bcd84887b2b03688c
ous776 <90853243+ous776@users.noreply.github.com>
2023-02-20 22:49:37 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,23 @@
 <template>
   <Header />
  <CountryList/>
+ <Footer/>
 
  
 </template>
 
 <script >
 import Header from "./components/Header.vue";
-import CountryList from "./components/CountryList.vue"
+import CountryList from "./components/CountryList.vue";
+import Footer from "./components/Footer.vue"
 
 export default {
   name: "App",
 
   components: {
     Header,
-    CountryList
+    CountryList,
+    Footer
   },
 
 };
---
+++ src/components/Footer.vue
@@ -0,0 +1,34 @@
+
+<template>
+    <footer>
+      <ul class="footer">
+        <li class="nav-item">
+          <a href="#" class="nav-link">Home</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">Features</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">FAQs</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">About</a>
+        </li>
+      </ul>
+      <p class="copy">&copy; 2021 Company, Inc</p>
+    </footer>
+  
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style scoped>
+
+</style>
